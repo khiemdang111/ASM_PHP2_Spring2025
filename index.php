ceaa0586548c0a788dcf393fcc10705977339a9b
khiemdang111 <dangkhiemct111@gmail.com>
2025-01-07 15:49:29 +0700

    source

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+session_start();
+ini_set('display_errors', '1');
+ini_set('display_startup_errors', '1');
+error_reporting(E_ALL);
+ini_set('log_errors', TRUE); 
+ini_set('error_log', './logs/php/php-errors.log');
+require_once 'vendor/autoload.php';
+
+
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+require_once 'config.php';
+include_once './Client/index.php';
